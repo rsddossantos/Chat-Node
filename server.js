5c6c34c6ffd84d291bcff92e1176c39bd22175d5
rodrigosantosipdv <rodrigo.santos@ipdvonline.com.br>
2022-02-10 16:14:00 -0300

    Implementando o socket do servidor e cliente

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 server.listen(3000);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 console.log("SERVIDOR CONECTADO NA PORTA 3000");
+
+let connectedUsers = [];
+
+io.on('connection', (socket) => {
+    console.log("Conexão detectada!");
+
+    socket.on('join-request', (username) => {
+        socket.username = username;
+        connectedUsers.push(username);
+        console.log(connectedUsers);
+        socket.emit('user-ok', connectedUsers);
+    });
+});
